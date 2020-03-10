ff8b9cf820ff2fe5b589f32c1ea17da6cacd7a9a
chungtwo <317281147@qq.com>
2020-03-10 10:43:52 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/android/coolweather/com/coolweather/db/City.java
@@ -0,0 +1,37 @@
+package android.coolweather.com.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2020/3/10.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private  String cityName;
+    private int provinId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinId() {
+        return provinId;
+    }
+
+    public void setProvinId(int provinId) {
+        this.provinId = provinId;
+    }
+}

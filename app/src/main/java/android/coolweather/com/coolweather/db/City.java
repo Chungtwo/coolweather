50e1903e56c1df6786fcaf1eb6aa0401d67f860d
chungtwo <317281147@qq.com>
2020-03-10 11:26:44 +0800

    未完成查询

--- app/src/main/java/android/coolweather/com/coolweather/db/City.java
+++ app/src/main/java/android/coolweather/com/coolweather/db/City.java
@@ -9,6 +9,7 @@ import org.litepal.crud.DataSupport;
 public class City extends DataSupport {
     private int id;
     private  String cityName;
+    private int cityCode;
     private int provinId;
 
     public int getId() {
@@ -33,5 +34,13 @@ public class City extends DataSupport {
 
     public void setProvinId(int provinId) {
         this.provinId = provinId;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
     }
 }
